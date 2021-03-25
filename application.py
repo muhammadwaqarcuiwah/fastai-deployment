c3b4c078d1652d26da7e6ad31aeff53d111ab53f
mwaqarsikandar <56826024+mwaqarsikandar@users.noreply.github.com>
2021-03-25 19:06:53 +0500

    ihih

--- application.py
+++ application.py
@@ -50,12 +50,12 @@ def upload_file():
     res =  zip (learn.data.classes, outputs.tolist())
     predictions = sorted(res, key=lambda x:x[1], reverse=True)
     top_predictions = predictions[0:1]
-    namme = pred_class.replace('___', 'CONDITION: ')
-    name = namme.replace('_', ' ')
-    for a, b in top_predictions:
-        bnb = a
-        outp = b
-    return jsonify(f'PLANT NAME: {name} CONFIDENCE: {top_predictions}')    
+    # namme = pred_class.replace('___', 'CONDITION: ')
+    # name = namme.replace('_', ' ')
+    # for a, b in top_predictions:
+    #     bnb = a
+    #     outp = b
+    return jsonify(f'PLANT NAME:  CONFIDENCE: {top_predictions}')    
     # pprint.pprint( top_predictions)
     # return img.resize(500)
    
