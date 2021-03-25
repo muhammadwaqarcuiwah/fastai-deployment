e777c475e48b57559c42a62f54196c324e43259f
mwaqarsikandar <56826024+mwaqarsikandar@users.noreply.github.com>
2021-03-25 21:33:19 +0500

    aqsq

--- application.py
+++ application.py
@@ -40,9 +40,9 @@ def upload_file():
     img = open_image(file)
     
     pred_class,pred_idx,outputs = learn.predict(img)
-    i = pred_idx.item()
-    classes = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
-    prediction = classes[i]
+    # i = pred_idx.item()
+    # classes = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
+    # prediction = classes[i]
     
     # def predict(url):
     # img = fetch_image(url)
@@ -51,14 +51,18 @@ def upload_file():
     predictions = sorted(res, key=lambda x:x[1], reverse=True)
     top_predictions = predictions[0:1]
     # plant = pred_class 
-    # namme = plant.replace('___', 'CONDITION: ')
-    # name1 = namme.replace('_', ' ')
-    # name2 = name1.replace('(', '')
-    # name = name2.replace(')', '')
+    namme = top_predictions.replace('___', '\n DISEASE: ')
+    name1 = namme.replace('_', ' ')
+    name2 = name1.replace('(', '')
+    name3 = name2.replace(')', '')
+    name4 = name3.replace('"', '')
+    name5 = name4.replace('[', '')
+    name6 = name5.replace(']', '')
+
     # for a, b in top_predictions:
     #     bnb = a
     #     outp = b
-    return jsonify(f'{top_predictions}')    
+    return jsonify(f'{name6}')    
     # pprint.pprint( top_predictions)
     # return img.resize(500)
    
